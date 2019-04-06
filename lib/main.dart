1bc4faf43ce7b9eb57d4291741ec779cc4d5429a
Shelby Drabant <drabants@gmail.com>
2019-04-05 18:13:51 -0700

    Added timer which counts up

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+enum dangerLevel {green, orange, red}
 
 void main() => runApp(MyApp());
 
@@ -6,30 +9,62 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home:MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-    );
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin{
+  AnimationController controller;
+  String get timerString {
+    Duration duration = controller.duration *controller.value;
+    return '${duration.inMinutes%60}:${(duration.inSeconds%60).toString().padLeft(2,'0')}';
   }
+  @override
+  void initState(){
+    super.initState();
+    controller = AnimationController(vsync: this, duration: Duration(hours:20));
+    controller.forward();
+  }
+
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.purpleAccent,
+      body: Center(
+        child: Container(
+          color: Colors.blue,
+          padding: EdgeInsets.all(25),
+          child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                child: AnimatedBuilder(
+                    animation: controller,
+                    builder: (BuildContext context, Widget child){
+                      return new Text(
+                        timerString,
+                        style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white),
+                      );
+                    }),
+              ),
+            ),
+          ],
+          ),
+        ),
+      ),
+    );
+
+  }
+
+}
+
+
+class meltdownButton{
+    
 }
